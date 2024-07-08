895692369d60474535f4a2e36c1e874751b51668
NicoV00 <6328@holbertonstudents.com>
2024-07-08 14:02:25 -0700

    added swapi star wars, already tested

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.error(err);
+  } else {
+    const films = JSON.parse(body).results;
+    let count = 0;
+
+    films.forEach((film) => {
+      const filterCharacters = film.characters.filter((character) =>
+        character.includes('18')
+      );
+      if (filterCharacters.length > 0) {
+        count++;
+      }
+    });
+
+    console.log(count);
+  }
+});
