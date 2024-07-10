dbeb84fb0bffa1dc7fd536289a797f04716bb40f
NicoV00 <6328@holbertonstudents.com>
2024-07-10 08:14:22 -0700

    added fixed task

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -2,7 +2,6 @@
 
 const request = require('request');
 const url = process.argv[2];
- 
 
 request(url, (err, res, body) => {
   if (err) {
@@ -12,9 +11,9 @@ request(url, (err, res, body) => {
   const cou = JSON.parse(body);
   const tasc = {};
   cou.forEach((tas) => {
-   if(tas.completed) {
-    tasc[tas.userId] = (tasc[tas.userId] || 0) + 1;
-   }
+    if (tas.completed) {
+      tasc[tas.userId] = (tasc[tas.userId] || 0) + 1;
+    }
   });
   console.log(tasc);
 });
