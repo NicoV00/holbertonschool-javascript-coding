52b9388b2ee608f74c89cee45d3ce994526c3f33
NicoV00 <6328@holbertonstudents.com>
2024-07-10 08:10:44 -0700

    added task 6 already tested

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+ 
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const cou = JSON.parse(body);
+  const tasc = {};
+  cou.forEach((tas) => {
+   if(tas.completed) {
+    tasc[tas.userId] = (tasc[tas.userId] || 0) + 1;
+   }
+  });
+  console.log(tasc);
+});
